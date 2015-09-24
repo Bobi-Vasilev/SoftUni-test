908a373d416fa76ee1dab9773e5317f2764e7c98
Bobi <Seishomaru@abv.bg>
2015-09-24 15:37:38 +0300

    Commit play

---
+++ WinBot/WinBot/WibBot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class WibBot
+{
+    private static void Main()
+    {
+        Console.Write("Enter how many win number you need: ");
+        int winNumber = int.Parse(Console.ReadLine());
+        Random rand = new Random();
+        for (int i = 1; i <= winNumber; i++)
+        {
+            int random = rand.Next(80) + 1;
+            Console.WriteLine("Wuner Numbers are : {0}", random);
+
+
+        }
+    }
+}
+        
+        
+        
+    
